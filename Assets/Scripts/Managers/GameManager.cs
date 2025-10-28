0e2598fa604279646b87e745b7f09d3247f243a3
EoSentry <kayanlucena28@gmail.com>
2025-10-27 23:54:51 -0300

    Nível 01 Adicionado

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,40 @@
+using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Player")]
+    public GameObject playerPrefab;
+
+    [Header("Enemies")]
+    public List<GameObject> enemiesList;
+
+    [Header("Reference")]
+    public Transform startPosition;
+
+    [Header("Animations")]
+    public float duration = .2f;
+    public float delay = .05f;
+    public Ease ease = Ease.OutBack;
+
+    private GameObject _currentPlayer;
+
+
+    private void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        _currentPlayer = Instantiate(playerPrefab);
+        _currentPlayer.transform.position = startPosition.position;
+        _currentPlayer.transform.DOScale(0, duration).From().SetEase(ease).SetDelay(delay);
+    }
+}
